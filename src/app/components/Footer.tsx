5ebcce691d806ee6f252b0858242dd06fc8ef7de
Santiago Dimaren <sdimaren1@gmail.com>
2023-10-12 18:00:33 -0400

    fixed links

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
           <a href="mailto:santiago.dimaren@gmail.com" className="text-gray-300 hover:text-gray-500 hover:mix-blend-color-dodge">Contact</a>
         </div>
         <div className="mt-2 mr-2">
-          <a target="_blank" href="linkedin.com/in/santiago-dimaren" className="text-gray-300 hover:text-gray-500 hover:mix-blend-color-dodge">Linkedin</a>
+          <a target="_blank" href="https://linkedin.com/in/santiago-dimaren" className="text-gray-300 hover:text-gray-500 hover:mix-blend-color-dodge">Linkedin</a>
         </div>
         <div className="mt-2 mr-2">
           <a target="_blank" href="https://github.com/sdimaren" className="text-gray-300 hover:text-gray-500 hover:mix-blend-color-dodge">Github</a>
